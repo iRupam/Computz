b17fafebdca9555f56d51426cd96b14cee303ed2
Rupam Medhi <27852445+iRupam@users.noreply.github.com>
2019-01-26 17:47:09 +0530

    Poorly written, help needed.
    I have spend a significant ampount of time writing this program. Are there any1 to solve it?

---
+++ major caode.c
@@ -0,0 +1,31 @@
+// The Build of Computz AI
+// 26012019
+
+//hidden data, ref computz lib.txt
+
+
+
+int fun(){
+    int x,y,z,temp;
+    temp=x+y+z;
+    return x;
+    };
+
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int num,ans;
+    printf("Enter a number of yours\n");
+    scanf("%d",&num);
+    num=fun(x);
+    ans=x+y+z;
+
+    scanf("%d");
+    if ans==1
+       printf("%d",Congrats, you are the only one left to save the aearth );
+       your role is(%d,ans)
+    }
+}
+
+
