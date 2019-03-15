3cfdfd7747f5e598da82f18bcaf517708b86550c
Rupam Medhi <27852445+iRupam@users.noreply.github.com>
2019-03-15 10:34:58 +0530

    Uploaded all C++ files developed.
    These programs are all written by me during my AimIT tenure.

---
+++ C++ codes/C++ Programs/array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int n[10],i;
+    for(i=0;i<10;i++)
+    {
+        cout<<"Enter number\n";
+        cin>>n[i];
+
+    }
+    cout<<"Printing\n";
+    for(i=0;i<10;i++)
+        cout<<"\n"<<n[i];
+return 0;
+}
---
+++ C++ codes/C++ Programs/login.c
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+    int password;
+    char em,email;
+    cout<<"Please enter your credentials\n";
+
+    //Input for ID
+    cout<<"Please enter your email id\n";
+    cin>>email;
+
+    //Input for Password
+    cout<<"Enter password\n";
+    cin>>password;
+
+    switch(em);
+    choice(rupam@computz.com):
+        printf("Correct Email");
+        break;
+
+
+    default: printf("Invalid ID\n");
+}
