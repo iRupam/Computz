24ef47c1a395e2f1261dfb13b73fd95b03c399b6
iRupam <rupamdhg@gmail.com>
2019-04-29 18:32:56 +0530

    from Ubuntu

--- major caode.c
+++ major caode.c
@@ -1,31 +1,25 @@
 // The Build of Computz AI
 // 26012019
-
 //hidden data, ref computz lib.txt
-
-
-
-int fun(){
-    int x,y,z,temp;
-    temp=x+y+z;
-    return x;
-    };
-
 #include<stdio.h>
 #include<math.h>
+int fun(int x){
+    int y=2,z=3,temp;
+    temp=x+y+z;
+    return temp;
+    }
 void main()
 {
     int num,ans;
     printf("Enter a number of yours\n");
     scanf("%d",&num);
-    num=fun(x);
-    ans=x+y+z;
+    ans=fun(num);
+    if(ans==1)
+       printf("%d Congrats,you are the only one left to save the earth\n",ans );
 
-    scanf("%d");
-    if ans==1
-       printf("%d",Congrats, you are the only one left to save the aearth );
-       your role is(%d,ans)
-    }
+    else
+        printf("%d, Try again",ans);
+
 }
 
 
