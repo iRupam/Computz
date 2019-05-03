8cd962425186de4495cfeec9e5178e53c8712860
iRupam <rupamdhg@gmail.com>
2019-05-03 15:03:52 +0530

    Added first string program

---
+++ C codes/str1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main( )
+{
+    int len;
+    char array[20]="fresh2refresh.com" ;
+ 
+    len = strlen(array) ;
+ 
+    printf ( "\string length  = %d \n" , len ) ;
+    return 0;
+}
+
