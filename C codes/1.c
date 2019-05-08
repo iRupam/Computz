21c7a6553d44f4bc525ef7383ed2cc19d662cc55
iRupam <rupamdhg@gmail.com>
2019-05-08 10:49:35 +0530

    Added string programs

---
+++ C codes/1.c
@@ -0,0 +1,15 @@
+
+// c program to demonstrate 
+// example of strlen() function. 
+  
+#include<stdio.h> 
+#include<string.h> 
+  
+int main() 
+{ 
+   char ch[]={'g', 'e', 'e', 'k', 's', '\0'}; 
+  
+   printf("Length of string is: %ld\n", strlen(ch)); 
+  
+ return 0; 
+}
