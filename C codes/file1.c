2f03313c4960bdc3a7fe2949aee228d168316ee9
Rupam Medhi <27852445+iRupam@users.noreply.github.com>
2020-03-24 00:54:26 +0530

    added "multiple file" c program
    Added file1.c and file2.c and compiiling instructions(todo.txt) which are separate files but part of same project.

---
+++ C codes/file1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void hello(void);
+int main(void) {
+	printf("I am from first file\n");
+	hello();
+	return 0;
+}
---
+++ C codes/file2.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void hello(void) {
+	printf("I'm from second file\n");
+	printf("Hello, World!\n");
+}
